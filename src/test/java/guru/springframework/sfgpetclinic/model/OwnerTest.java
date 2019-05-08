51a1d2c7a0cbefa5f24a79c61d6ffcf69c3469d7
Ralph L <lettau.ralph@gmail.com>
2019-05-08 21:17:39 +0200

    Add Hamcrest

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnerTest {
@@ -26,5 +28,7 @@ class OwnerTest {
                             () -> assertEquals("123456789", owner.getTelephone())
                     );
                 });
+
+        assertThat(owner.getCity(), is("Key Largo"));
     }
 }
