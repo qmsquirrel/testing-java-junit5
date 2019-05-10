3c7565fe34f59775b620f687679de0c1b6e61ed0
Ralph L <lettau.ralph@gmail.com>
2019-05-10 23:09:26 +0200

    Nested Tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,101 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Owner Map ServiceTest -")
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+    PetTypeService petTypeService;
+    PetService petService;
+
+    @BeforeEach
+    void setUp() {
+        petService = new PetMapService();
+        petTypeService = new PetTypeMapService();
+        ownerMapService = new OwnerMapService(petTypeService, petService);
+    }
+
+    @DisplayName("Verify zero owners")
+    @Test
+    void ownersAreZero() {
+
+        int ownerCount = ownerMapService.findAll().size();
+
+        assertThat(ownerCount).isEqualTo(0);
+
+    }
+
+    @DisplayName(" Pet type - ")
+    @Nested
+    class TestCreatePetTypes {
+
+        @BeforeEach
+        void setUp() {
+            PetType petType = new PetType(1L, "Dog");
+            PetType petType2 = new PetType(2L, "Cat");
+            petTypeService.save(petType);
+            petTypeService.save(petType2);
+        }
+
+        @Test
+        void testPetCount() {
+            int petTypeCount = petTypeService.findAll().size();
+
+            assertThat(petTypeCount).isNotZero().isEqualTo(2);
+        }
+
+        @DisplayName("Save Owner Test -")
+        @Nested
+        class SaveOwnerTest {
+
+            @BeforeEach
+            void setUp() {
+                ownerMapService.save(new Owner(1L, "Before", "Each"));
+            }
+
+            @Test
+            void saveOwner() {
+                Owner owner = new Owner(2L, "Joe", "Buck");
+
+                Owner savedOwner = ownerMapService.save(owner);
+
+                assertThat(savedOwner).isNotNull();
+            }
+
+            @DisplayName("Find Owners Test - ")
+            @Nested
+            class FindOwnerTest {
+
+                @DisplayName("Find Owner")
+                @Test
+                void findOwner() {
+
+                    Owner foundOwner = ownerMapService.findById(1L);
+
+                    assertThat(foundOwner).isNotNull();
+                }
+
+                @DisplayName("Find Owner Not Found")
+                @Test
+                void findOwnerNotFound() {
+
+                    Owner foundOwner = ownerMapService.findById(2L);
+
+                    assertThat(foundOwner).isNull();
+
+                }
+            }
+        }
+
+    }
+}
