e2cb14205dd6b3eaf2f2cc79cea5a28468eb6abb
Ralph L <lettau.ralph@gmail.com>
2019-05-10 17:07:28 +0200

    Test for Model Content

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.mocks.ModelMock;
-import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
@@ -10,26 +9,32 @@ import guru.springframework.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class VetControllerTest {
 
     VetController vetController;
-    Model mockModel;
+    ModelMock mockModel;
+    Set<Speciality> specs;
+    Vet vet1;
+    Vet vet2;
 
     @BeforeEach
     void setUp() {
         VetService vetService = new VetMapService(new SpecialityMapService());
         Speciality spec1 = new Speciality("Dogs");
         Speciality spec2 = new Speciality("Cats");
-        Set<Speciality> specs = new HashSet<>();
+        specs = new HashSet<>();
         specs.add(spec1);
         specs.add(spec2);
-        Vet vet1 = new Vet(1l, "John", "Baker",null);
-        Vet vet2 = new Vet(2l, "Lisa", "Smith", specs);
+        vet1 = new Vet(1l, "John", "Baker",specs);
+        vet2 = new Vet(2l, "Lisa", "Smith", new HashSet<>());
+        vetService.save(vet1);
+        vetService.save(vet2);
         vetController = new VetController(vetService);
         mockModel = new ModelMock();
 
@@ -40,6 +45,18 @@ class VetControllerTest {
         String result = vetController.listVets(mockModel);
         assertEquals("vets/index", result, "Controller gives back correct name");
 
+        HashMap<String, Object> mockData = mockModel.getMockData();
+        assertEquals(mockData.size(), 1, "Only one thing added to scope");
+        assertNotNull(mockData.get("vets"),  "There is a vet entry");
+        Set<Vet> vets = (Set<Vet>) mockData.get("vets");
+        assertEquals(vets.size(), 2, "There are two vets");
+        assertAll("Testing the vets",
+                () -> {
+                    assertTrue(vets.contains(vet1), "Vet1 is in the set");
+                },
+                () -> {
+                    assertTrue(vets.contains(vet2), "Vet2 is in the set");
+                });
 
     }
 }
