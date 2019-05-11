2b7939f4f2d7cd43276e1ef1fa030301f6335c91
Ralph L <lettau.ralph@gmail.com>
2019-05-11 13:47:25 +0200

    Added default beforeEach to ModelTest Interface

--- src/test/java/guru/springframework/sfgpetclinic/ModelTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/ModelTest.java
@@ -1,7 +1,17 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInfo;
 
 @Tag("model")
 public interface ModelTest {
+
+    @BeforeEach
+    default void beforeEach(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": "
+                + repetitionInfo.getCurrentRepetition() + " of "
+                + repetitionInfo.getTotalRepetitions());
+    }
 }
